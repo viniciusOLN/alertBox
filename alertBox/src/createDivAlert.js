319f958a9354da04da1d06b8a5454c7673696bb4
Vinicius Oliveira <vinicius853o@gmail.com>
2021-07-17 19:17:47 -0300

    adding the initial div to use as notification in the index

--- alertBox/src/createDivAlert.js
+++ alertBox/src/createDivAlert.js
@@ -1,11 +1,11 @@
 //creating a div to use and insert all type of data
 
 export function createDivAlert(css_class) {
-    let divCreator = function (class_css){
+    const divCreator = function (class_css){
       let newElement = document.createElement("div")
       let newNode = document.body.appendChild(newElement)
       newNode.setAttribute("class", class_css)
     }
-    
+
     divCreator(css_class)
 }
